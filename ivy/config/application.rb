0d7e4b71b6ff78156ccc81a1d04e3d77a9eaed5b
Anthony Williams <hi@antw.io>
2015-02-16 16:12:44 +0000

    Move Ivy paths into the main application file

--- ivy/config/application.rb
+++ ivy/config/application.rb
@@ -13,8 +13,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require_relative '../lib/ivy'
-
 module Ivy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -29,4 +27,39 @@ module Ivy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-end
+
+  # Public: Path to the directory in which static data files files typically
+  # reside. This will normally have subfolders like curves/, technologies/, etc.
+  #
+  # Returns a Pathname.
+  def self.data_dir
+    @data_dir ||= Rails.root.join('data')
+  end
+
+  # Public: Sets the path to the direction in which the data files reside.
+  #
+  # Returns the path provided.
+  def self.data_dir=(path)
+    path = path.is_a?(Pathname) ? path : Pathname.new(path.to_s)
+    path = Rails.root.join(path) if path.relative?
+
+    Rails.cache.clear
+
+    @data_dir = path
+  end
+
+  # Public: Wrap around a block of code to work with a temporarily altered
+  # +data_dir+ setting.
+  #
+  # directory - The new, but temporary, data_dir path.
+  #
+  # Returns the result of your block.
+  def self.with_data_dir(directory)
+    previous      = data_dir
+    self.data_dir = directory
+
+    yield
+  ensure
+    self.data_dir = previous
+  end
+end # Ivy
