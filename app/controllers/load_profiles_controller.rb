7eecf545609967b860556fbf39ce3d6df579f184
Anthony Williams <hi@antw.io>
2015-03-26 15:06:00 +0000

    Add "delete profile" button and action

--- app/controllers/load_profiles_controller.rb
+++ app/controllers/load_profiles_controller.rb
@@ -2,6 +2,8 @@ class LoadProfilesController < ApplicationController
   respond_to :html
   respond_to :json, only: :show
 
+  before_filter :fetch_load_profile, except: [:index, :new, :create]
+
   # GET /load_profiles
   def show
     respond_with(@load_profile = LoadProfile.find(params[:id]))
@@ -19,20 +21,30 @@ class LoadProfilesController < ApplicationController
 
   # GET /load_profiles/:id/edit
   def edit
-    @load_profile = LoadProfile.find(params[:id])
+    # @load_profile = LoadProfile.find(params[:id])
   end
 
   # PATCH /load_profiles/:id
   def update
-    @load_profile = LoadProfile.find(params[:id])
+    # @load_profile = LoadProfile.find(params[:id])
     @load_profile.update_attributes(load_profile_params)
 
     respond_with(@load_profile)
   end
 
+  # DELETE /load_profiles/:id
+  def destroy
+    @load_profile.destroy
+    redirect_to(load_profiles_url)
+  end
+
   #######
   private
   #######
+
+  def fetch_load_profile
+    @load_profile = LoadProfile.find(params[:id])
+  end
 
   def load_profile_params
     lp_params = params.require(:load_profile).permit(
