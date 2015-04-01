4b98ca2643aa1779abdc3762f07023673dc60c54
Anthony Williams <hi@antw.io>
2015-04-01 17:27:46 +0100

    Remove unnecessary Technology::IMPORT_ATTRIBUTES

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,13 +1,4 @@
 class Technology < ActiveRecord::Base
-  # Defines attributes which may be read from ETEngine and imported into the
-  # testing ground. Each key is an attribute provided by ETE, and the value is
-  # the local attribute to which it maps.
-  IMPORT_ATTRIBUTES = {
-    demand:                      :demand,
-    electricity_output_capacity: :capacity,
-    input_capacity:              :capacity
-  }.freeze
-
   validates :key,
     presence: true,
     length: { maximum: 100 },
@@ -19,7 +10,9 @@ class Technology < ActiveRecord::Base
 
   validates :import_from,
     length: { maximum: 50 },
-    inclusion: { in: IMPORT_ATTRIBUTES.keys.map(&:to_s), allow_blank: true }
+    inclusion: {
+      in: Import::TechnologyBuilder::ATTRIBUTES.keys,
+      allow_blank: true }
 
   validates :export_to,
     length: { maximum: 100 }
