d8ed75307627caf78c5f360d34e8db7e3b94a2fe
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-21 10:34:56 +0200

    redirect to sign in page when not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :require_login
   before_filter :load_recent_testing_grounds
 
   def load_recent_testing_grounds
     @recent_testing_grounds = TestingGround.all.order('created_at DESC').limit(5)
   end
+
+  private
+
+    def require_login
+      unless current_user
+        redirect_to new_session_path
+      end
+    end
 end
