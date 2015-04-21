e409b702edc629ae893e164939d2f5d79ae9b644
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-21 14:15:59 +0200

    adding an etmodel base url in the configuration files

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,4 +3,6 @@ require File.expand_path(File.dirname(__FILE__)) + '/production'
 Rails.application.configure do
   # Temporarily use in-memory caching.
   config.cache_store = :memory_store, { size: 64.megabytes }
+
+  ET_MODEL_URL = "beta.pro.et-model.com"
 end
