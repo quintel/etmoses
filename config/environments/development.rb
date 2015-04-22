ab369c8371dd6ee99265a9938914bc69196f3ed4
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-22 15:33:25 +0200

    using mailcather settings for development environment mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
