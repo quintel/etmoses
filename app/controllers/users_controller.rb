e0b4d6aac2a80abf9ef1316e4e7a5e610a3dc6c1
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-22 15:32:12 +0200

    adding correct parameters to registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   skip_before_filter :require_login
 
   def new
-    @registration_form = RegistrationForm.new
+    @registration_form = RegistrationForm.new(params[:registration_form])
   end
 
   def create
-    @registration_form = RegistrationForm.new(params[:user])
+    @registration_form = RegistrationForm.new(params[:registration_form])
     if @registration_form.submit
       flash[:notice] = "Your account needs activation before you can sign in."
       redirect_to root_path
     else
-      flash[:alert] = @registration_form.errors.messages
+      flash[:alert] = @registration_form.errors.full_messages.join(", ")
       render :new
     end
   end
