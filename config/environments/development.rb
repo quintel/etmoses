6527e1fc684b9d10b994ea478598ebcbe5ea4c4d
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-04-28 15:06:02 +0200

    using smtp as the mail protocol for development environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
   config.eager_load                          = false
   config.consider_all_requests_local         = true
   config.action_controller.perform_caching   = false
+  config.action_mailer.delivery_method       = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation          = :log
   config.active_record.migration_error       = :page_load
