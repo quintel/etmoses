08a90dad07d13712d142eab4d3d9f0e4ea3d191e
Anthony Williams <hi@antw.io>
2015-05-05 17:07:45 +0100

    Rename Network::Path#source to "leaf"
    It should, in fact, have been called "sink", but "leaf" is more
    descriptive anyway...

--- app/models/network/path.rb
+++ app/models/network/path.rb
@@ -1,33 +1,39 @@
 module Network
   # Describes a path of nodes from a technology node, back to the root.
   class Path
-    # Public: Given a source node, returns a Path which represents the route
+    # Public: Given a leaf node, returns a Path which represents the route
     # back to the root node.
     #
     # Returns a Path.
-    def self.find(source)
-      new([source, *source.ancestors.to_a])
+    def self.find(leaf)
+      new([leaf, *leaf.ancestors.to_a])
     end
 
     # Internal: Creates a new Path, using the given array of nodes to describe
     # the path through the parents to the root node.
     def initialize(nodes)
-      @path   = nodes
-      @source = nodes.first
+      @path = nodes
+      @leaf = nodes.first
+    end
+
+    # Public: The path as an array, with the leaf node first, with each node
+    # leading to the root.
+    def to_a
+      @path.to_a
     end
 
     # Public: The conditional consumption being created by the consumption node.
     #
     # Returns a numeric.
     def conditional_consumption_at(point)
-      @source.conditional_consumption_at(point)
+      @leaf.conditional_consumption_at(point)
     end
 
     # Public: The mandatory consumption being created by the consumption node.
     #
     # Returns a numeric.
     def mandatory_consumption_at(point)
-      @source.mandatory_consumption_at(point)
+      @leaf.mandatory_consumption_at(point)
     end
 
     # Public: Sends a given amount of energy down the path, increasing the
@@ -37,8 +43,8 @@ module Network
     def consume(point, amount)
       return if amount.zero?
 
-      if @source.consumption_at(point) >= mandatory_consumption_at(point)
-        @source.assign_conditional_consumption(point, amount)
+      if @leaf.consumption_at(point) >= mandatory_consumption_at(point)
+        @leaf.assign_conditional_consumption(point, amount)
       end
 
       @path.each { |node| node.consume(point, amount) }
