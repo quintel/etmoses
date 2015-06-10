96f16ec7d1964871e10c8200f49314d2efbbdb77
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-06-10 14:23:41 +0200

    Removing jquery safe

--- config/initializers/string.rb
+++
@@ -1,9 +0,0 @@
-class String
-  #
-  # Removes all possible characters that can be interpreted as classes or id's
-  # in a jQuery selector.
-  #
-  def jquery_safe
-    self.gsub(/\.|\#/, '')
-  end
-end
