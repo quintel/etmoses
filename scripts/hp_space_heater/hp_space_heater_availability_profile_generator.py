37c05b09f04e2efc7c6080753a56c9f0dab08615
Chael Kruip <chael.kruip@quintel.com>
2015-06-15 17:29:44 +0200

    Updated heat pump script to include insulation and solar irradiation

--- scripts/hp_space_heater/hp_space_heater_availability_profile_generator.py
+++ scripts/hp_space_heater/hp_space_heater_availability_profile_generator.py
@@ -2,44 +2,17 @@
 """
 Created on Wed Apr 29 11:09:48 2015
 
-@author: jorisberkhout
+@author: joris.berkhout@quintel.com & chael.kruip@quintel.com
 """
 
-#==============================================================================
-# This script can be used to generate typical domestic hot water (DHW) profiles
-# for a period of one year and time steps of 15 minutes. The script is based in
-# great part on Realistic Domestic Hot-Water Profiles in Different Time Scales,
-# Jordan (2001). This study assumes a daily average DHW use of 200 liters and
-# distinguishes four types of DHW consumption, each with an associated volume
-# and average daily occurence:
-#
-# type A: short load (1 liter per event, 28 occurences per day)
-# type B: medium load (6 liter per event, 12 occurences per day)
-# type C: bath (140 liter per event, 0.143 occurences per day (once a week))
-# type D: shower (40 liter per event, 2 occurences per day)
-#
-# According to Jordan (2001), the duration of each of these types is shorter 
-# than 15 minutes (i.e. the time resolution of our simulation). Hence we 
-# decided to only model the probability that an event occurs within each 15 
-# minute time step and assign the entire volume of that event to that 15 minute
-# bin. The probability of each type of event varies throughout the year (i.e.
-# slightly more DHW consumption in winter), throughout the week (more in the
-# weekend) and throughout the day (no DHW consumption during the night).
-# 
-# The script returns randomly generated profiles with a time resolution of 15 
-# minutes. To match the needs of the ETM these exported profiles are scaled to
-# the maximal storage volume of the electric boiler used in the ETM for P2H.
-# (i.e. 9.3 kWh)
-#==============================================================================
-
 import numpy as np
-import pylab as plt
-#import heat_pump_classes
-#reload (heat_pump_classes)
 from numpy import genfromtxt
+import pylab as plt
+import os
+import heat_pump_classes
+reload (heat_pump_classes)
  
 plt.close()
- 
  
 #==============================================================================
 # This script generates load profiles for heat pumps based on a temperature
@@ -47,9 +20,6 @@ plt.close()
 #==============================================================================
  
 # Global constants
-year = 2013
-kWh_per_degree_day = 3.0
-days_per_hour = 0.04166666667
  
 # Specs of heat-pump
 capacity_in_kW = 3
@@ -63,35 +33,40 @@ energy_per_liter_per_degree = 0.00116
  
 # Output the capacity of the storage tank
 max_content_in_kWh = liters * energy_per_liter_per_degree * max_temperature
-print "Max energy content of tank:", max_content_in_kWh, " kWh"
  
 # Data
-temperature_data =genfromtxt("/Users/jorisberkhout/Dropbox (Quintel)/201405_alliander_model_shared/profiles/bilt_2013_T.txt", delimiter=" ")
+temperature_data =genfromtxt(os.getcwd() + "/../input_data/bilt_2013_T.txt", delimiter=" ")
  
 # Extract only temperature data and converting to degrees C
 temperatures = np.array(zip(*temperature_data)[2]) / 10.0       
  
-# How many kWh do I have to heat for in this hour?
-def heat_demand(degree_days):
-    return degree_days * kWh_per_degree_day
- 
-# Return the degree days for this hour
-def degree_days(outside_temperature, thermostat_temperature):
-    if outside_temperature < thermostat_temperature:
-        return days_per_hour * (thermostat_temperature - outside_temperature)
-    else:
-        return 0.0
-        
+# Solar data
+irradiance_data = genfromtxt(os.getcwd() + "/../input_data/irradiance_in_watt_per_m_squared_maastricht.csv", delimiter=" ")
+irradiance_in_kW = irradiance_data / 1000.0
+
+# Specs of house
+floor_area = 100
+window_area = 20
+thermostat_temperature = 20
+
+house = heat_pump_classes.house(floor_area, 
+                                window_area, 
+                                thermostat_temperature)                          
+# Print info about house
+house.print_info()
+
+# This function creates an 'availability profile' from a 'use profile'. 
+# 
+# Input: use profile (the use of hot water as a function of time) [fraction of storage tank for every 15 minutes]
+# Output: availability profile (which fraction of the total storage volume of the heat pump boiler should be full for every 15 minutes)
+# [fraction of storage tank for every 15 minutes]
+#
 def create_availability_profile(use_profile):
-    # This profile creates an availability profile from a use profile. The use
-    # of hot water determines when the heat pump needs to start heating. The
-    # output profile of this function indicates which fraction of the total 
-    # storage volume of the heat pump boiler should be full
 
     # Calculate which fraction of the boilers capacity you can fill in 15 minute
     # 3600 is the conversion factor J / Wh    
-    #added_energy = capacity_in_kW / 4.0  / max_content_in_kWh
-    added_energy = 0.028   
+    time_steps_per_hour = 4.0 # currently we work with 15 minute intervals
+    added_energy = capacity_in_kW / time_steps_per_hour / max_content_in_kWh
     print added_energy
 
     # start at the end of the year and works backwards
@@ -122,56 +97,46 @@ def create_availability_profile(use_profile):
  
 # create the demand profiles
 
-demand_profile = []
- 
+# Initialise profiles
+heat_demand_profile = []
+
+
 # Main loop
-thermostat_temperature = 20.0
- 
-#i = 0
+i = 0
 for temperature in temperatures:
-     
-#    i += 1
-#    if i > 50:
-#        sys.exit(0)
- 
- 
-    # Leak some heat from the tank
-#    storage_tank.leak()
-     
-    # Calculate degree days for this hour
-    current_degree_days = degree_days(temperature, thermostat_temperature)
-     
-    # Calculate the demand
-    current_heat_demand = heat_demand(current_degree_days)
     
-    demand_profile.append(current_heat_demand)
+    i += 1
+    
+    # Effect of a time-dependent thermostate:
+    # Only between 08:00 and 00:00 to we heat the house
+    #if 8 < (i % 24):
+    #   temperature = thermostat_temperature
+
+    # Leak heat from the house (if it is indeed colder outside than inside) 
+    # Also allow sunlight to shine in and heat the house
+    house.exchange_heat_with_outside(temperature, np.mean(irradiance_in_kW[4*i:4*i+4]))
+        
+    # Calculate the demand
+    current_heat_demand = house.get_heat_demand()
+    
+    if current_heat_demand > 0.0:
+        heat_demand_profile.append(current_heat_demand)
+    else:
+        heat_demand_profile.append(0.0)
 
 # scale the demand_profile
-demand_profile = np.array(demand_profile)
+demand_profile = np.array(heat_demand_profile)
 demand_profile = demand_profile / max_content_in_kWh
-
-# Global variables
-
-
-    
-# main loop    
-    
-    
-use_filename = 'dhw_use_profile.csv'
-#plt.savetxt(use_filename, pattern, fmt='%.3f', delimiter=',')    
     
 # calculate the availability profile from the use profile
 availability_profile = create_availability_profile(demand_profile)    
     
-filename = 'dhw_availability_profile.csv'
+filename = 'hp_sh_availability_profile.csv'
 #plt.savetxt(availability_filename, availability_profile, fmt='%.3f', delimiter=',')    
-    
     
 plt.plot(demand_profile)
 plt.plot(availability_profile)
     
-#plt.plot(availability_profile)
-
 # bin all events per day and display these
 #plt.plot(pattern.reshape(-1, 96).sum(axis=1), 'k-')
 #plt.xlabel('time (days)')
