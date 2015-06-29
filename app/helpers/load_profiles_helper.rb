e17d2734825557efc80a1280b0a743214699110c
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-06-29 09:52:20 +0200

    Schema bump + removing a bit of whitespace

--- app/helpers/load_profiles_helper.rb
+++ app/helpers/load_profiles_helper.rb
@@ -1,7 +1,7 @@
 module LoadProfilesHelper
   def load_profile_categories_select_options
     options_for_select(LoadProfiles::Hierarchy.new.tree_sort.map do |l|
-      ["#{ " - " * l[:path_size] }#{ l[:load_profile_category].name }",
+      ["#{ "- " * l[:path_size] }#{ l[:load_profile_category].name }",
         l[:load_profile_category].id]
     end)
   end
