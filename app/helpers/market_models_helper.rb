d1270d41f6082b24754e1fffe86f72eceba9f28a
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-06-30 10:55:13 +0200

    Splitting up the form into two tables

--- app/helpers/market_models_helper.rb
+++ app/helpers/market_models_helper.rb
@@ -1,9 +1,9 @@
 module MarketModelsHelper
-  def stakeholder_options
-    options_for_select(Stakeholder.all.map(&:name))
+  def stakeholder_options(options = {})
+    options_for_select(Stakeholder.all.map(&:name), options)
   end
 
-  def tariff_options
-    options_for_select(MarketModel::BASES)
+  def tariff_options(options = {})
+    options_for_select(MarketModel::BASES, options)
   end
 end
