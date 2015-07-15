dc419b3b1b6b2607355be269e297ef72ba3134a5
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-07-15 14:12:33 +0200

    Adding measure to market models

--- app/models/market_model.rb
+++ app/models/market_model.rb
@@ -2,6 +2,7 @@ class MarketModel < ActiveRecord::Base
   include Privacy
 
   FOUNDATIONS = ["kWh", "kW_connection", "kW_max", "kW_contracted", "kW_flex"]
+  MEASURES = ['per_hh', 'endpoints_stakeholder_total']
   DEFAULT_INTERACTIONS = JSON.dump([{ "stakeholder_from" => "",
                                       "stakeholder_to"   => "",
                                       "tariff"           => "",
