c57d1b24fe6990a3ee83090c1440e35865360d62
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-01 10:51:14 +0200

    Adding applied stakeholder property to market model interactions

--- app/models/market_model.rb
+++ app/models/market_model.rb
@@ -3,10 +3,12 @@ class MarketModel < ActiveRecord::Base
 
   FOUNDATIONS = ["kWh", "kW_connection", "kW_max", "kW_contracted", "kW_flex"]
   MEASURES = ['per_hh', 'endpoints_stakeholder_total']
-  DEFAULT_INTERACTIONS = [{ "stakeholder_from" => "",
-                            "stakeholder_to"   => "",
-                            "tariff"           => "",
-                            "price"            => "" }]
+  PRESENTABLES = %w(stakeholder_from stakeholder_to foundation applied_stakeholder tariff)
+  DEFAULT_INTERACTIONS = [{ "stakeholder_from"    => "",
+                            "stakeholder_to"      => "",
+                            "tariff"              => "",
+                            "applied_stakeholder" => "",
+                            "price"               => "" }]
 
   belongs_to :user
 
