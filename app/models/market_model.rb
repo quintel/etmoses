3336b055843c714d4f543ac44b9dd14e07b5c6f5
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-01 09:46:45 +0200

    Removing JSON dump for default interactions

--- app/models/market_model.rb
+++ app/models/market_model.rb
@@ -3,10 +3,10 @@ class MarketModel < ActiveRecord::Base
 
   FOUNDATIONS = ["kWh", "kW_connection", "kW_max", "kW_contracted", "kW_flex"]
   MEASURES = ['per_hh', 'endpoints_stakeholder_total']
-  DEFAULT_INTERACTIONS = JSON.dump([{ "stakeholder_from" => "",
-                                      "stakeholder_to"   => "",
-                                      "tariff"           => "",
-                                      "price"            => "" }])
+  DEFAULT_INTERACTIONS = [{ "stakeholder_from" => "",
+                            "stakeholder_to"   => "",
+                            "tariff"           => "",
+                            "price"            => "" }]
 
   belongs_to :user
 
