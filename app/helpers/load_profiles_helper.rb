ce7eca67bc0c88514ec0a1bc6d1114ccdd2df20d
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-02 10:46:33 +0200

    Remember the chosen load profile category

--- app/helpers/load_profiles_helper.rb
+++ app/helpers/load_profiles_helper.rb
@@ -1,11 +1,13 @@
 module LoadProfilesHelper
-  def load_profile_categories_select_options
-    options_for_select(LoadProfiles::Hierarchy.new.tree_sort.map do |category|
+  def load_profile_categories_select_options(profile)
+    load_profiles = LoadProfiles::Hierarchy.new.tree_sort.map do |category|
       [ "#{ "- " * category[:path_size] }#{ category[:load_profile_category].name }",
         category[:load_profile_category].id,
         { data: { curve_type: category[:load_profile_category].key } }
       ]
-    end)
+    end
+
+    options_for_select(load_profiles, { selected: profile.load_profile_category_id })
   end
 
   def technologies_select_options(selected = nil)
