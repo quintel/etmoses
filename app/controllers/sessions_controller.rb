e06e64b2ed4dc3817ff78e4b7c92e742f2877c87
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-02 11:33:56 +0200

    Validate password of user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < Devise::SessionsController
   def create
     user = User.find_by_email(params[:session][:email])
 
-    if user && user.activated? && sign_in(user)
+    if user && user.activated? && user.valid_password?(params[:session][:password]) && sign_in(user)
       redirect_to signed_in_redirect_path
     elsif user && !user.activated?
       flash.now[:alert] = "Account is not activated"
