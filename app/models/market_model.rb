57a12ce898908688f5e0241bf880b8ba6f6cc45f
Anthony Williams <hi@antw.io>
2015-09-08 16:54:45 +0100

    Remove unused MarketModel::MEASURES constant

--- app/models/market_model.rb
+++ app/models/market_model.rb
@@ -2,7 +2,6 @@ class MarketModel < ActiveRecord::Base
   include Privacy
 
   FOUNDATIONS = %w(connections kWh kW_connection kW_max kW_contracted kW_flex)
-  MEASURES = ['per_hh', 'endpoints_stakeholder_total']
   PRESENTABLES = %w(stakeholder_from stakeholder_to foundation applied_stakeholder tariff)
   DEFAULT_INTERACTIONS = [{ "stakeholder_from"    => "",
                             "stakeholder_to"      => "",
