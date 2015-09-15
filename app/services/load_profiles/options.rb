ef833e20dd9c7dd9c6639fcdc00514175273fc49
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-15 16:06:17 +0200

    Fixing selection bug

--- app/services/load_profiles/options.rb
+++ app/services/load_profiles/options.rb
@@ -4,7 +4,7 @@ module LoadProfiles
     # Class to generate options for load profiles
     #
     def initialize(load_profiles, testing_ground, technology)
-      @load_profiles = load_profiles[technology] || LoadProfile.order(:key)
+      @load_profiles  = selected_load_profiles(load_profiles, technology)
       @testing_ground = testing_ground
     end
 
@@ -17,6 +17,14 @@ module LoadProfiles
     end
 
     private
+
+    def selected_load_profiles(load_profiles, technology)
+      if technology
+        load_profiles[technology.key.to_s] || []
+      else
+        LoadProfile.order(:key)
+      end
+    end
 
     def profile_ids
       @profile_ids ||= @testing_ground.technology_profile.list.values.flatten
