ef2d53097b18b17203a2d2b97bf788a72176a820
Gerard Westerhof <gerard.westerhof@quintel.com>
2015-09-16 12:14:54 +0200

    Adding Freeform row if financials are all blank

--- app/helpers/business_case_helper.rb
+++ app/helpers/business_case_helper.rb
@@ -1,7 +1,11 @@
 module BusinessCaseHelper
   def finance_table_freeform_row(business_case)
-    (business_case.financials.detect{|t| t['freeform'] } ||
-     BusinessCase::FREEFORM_ROW).values.flatten
+    if business_case.financials
+      (business_case.financials.detect{|t| t['freeform'] } ||
+       BusinessCase::FREEFORM_ROW).values.flatten
+    else
+      BusinessCase::FREEFORM_ROW.values.flatten
+    end
   end
 
   def finance_table_rows(business_case)
