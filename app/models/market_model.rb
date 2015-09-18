c497c49223398782e6979b0ae398b89c5c47b40b
Anthony Williams <hi@antw.io>
2015-09-18 14:27:39 +0100

    Add flexibility measures to the front-end

--- app/models/market_model.rb
+++ app/models/market_model.rb
@@ -1,7 +1,11 @@
 class MarketModel < ActiveRecord::Base
   include Privacy
 
-  FOUNDATIONS = %w(connections kWh kW_connection kW_max kW_contracted kW_flex)
+  FOUNDATIONS = %w(
+    connections kWh kW_connection kW_max kW_contracted
+    flex_realised flex_potential
+  )
+
   PRESENTABLES = %w(stakeholder_from stakeholder_to foundation applied_stakeholder tariff)
   DEFAULT_INTERACTIONS = [{ "stakeholder_from"    => "",
                             "stakeholder_to"      => "",
