105e9bc5f32f23791817dead345e2a866bcd2992
Anthony Williams <hi@antw.io>
2015-09-22 13:53:49 +0100

    Show curve type in interaction table

--- app/helpers/market_models_helper.rb
+++ app/helpers/market_models_helper.rb
@@ -14,4 +14,12 @@ module MarketModelsHelper
   def financial_profile_options(options = {})
     options_for_select(PriceCurve.all.map(&:key), options)
   end
+
+  def format_interaction_tariff(interaction)
+    type = t("tariff.type.#{ interaction['tariff_type'] }")
+
+    [ content_tag(:span, type, class: 'tariff_type label label-default'),
+      content_tag(:span, interaction['tariff'], class: 'tariff')
+    ].join(' ').html_safe
+  end
 end
