5e397e5bbe7dd2381c09f0f30220f4a8314b07de
Anthony Williams <hi@antw.io>
2015-09-25 16:48:23 +0100

    Only import costs from existing ETEngine technologies

--- db/migrate/20150925142359_add_initial_investment_technical_lifetime_attributes_to_db.rb
+++ db/migrate/20150925142359_add_initial_investment_technical_lifetime_attributes_to_db.rb
@@ -1,6 +1,17 @@
 class AddInitialInvestmentTechnicalLifetimeAttributesToDb < ActiveRecord::Migration
   def change
-    Technology.where("`key` != 'transport_car_using_electricity'").map do |technology|
+    techs = %w(
+      energy_flexibility_p2g_electricity
+      households_flexibility_p2h_electricity
+      households_flexibility_p2p_electricity
+      households_solar_pv_solar_radiation
+      households_space_heater_heatpump_air_water_electricity
+      households_space_heater_heatpump_ground_water_electricity
+      households_water_heater_heatpump_air_water_electricity
+      households_water_heater_heatpump_ground_water_electricity
+    )
+
+    Technology.where(key: techs).map do |technology|
       technology.importable_attributes.create(name: 'initial_investment')
       technology.importable_attributes.create(name: 'technical_lifetime')
     end
