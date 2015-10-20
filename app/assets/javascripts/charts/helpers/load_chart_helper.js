a1184b851ad77616a12ca8330383fdc82d66e55a
Gerard Westerhof <gerard@grdw.nl>
2015-10-20 10:35:45 +0200

    Fixes #543
    - Also includes 'use strict' fixes

--- app/assets/javascripts/charts/helpers/load_chart_helper.js
+++ app/assets/javascripts/charts/helpers/load_chart_helper.js
@@ -1,67 +1,72 @@
-LoadChartHelper = {
-  globalBrushExtent: undefined,
-  currentWeek: undefined,
-  forceReload: false,
-  disableCapacity: true,
-  charts: [],
+var LoadChartHelper = (function () {
+    'use strict';
 
-  toggleCapacity: function(currentChartId){
-    currentChart = this.charts[currentChartId - 1];
-    d3Chart = d3.select(".chart-id-" + currentChartId + " svg");
-    d3ChartData = d3Chart.data();
+    return {
+        globalBrushExtent: undefined,
+        currentWeek: undefined,
+        forceReload: false,
+        disableCapacity: true,
+        charts: [],
 
-    if(d3ChartData[0]){
-      d3ChartData[0][d3ChartData[0].length - 1]['disabled'] = this.disableCapacity;
+        toggleCapacity: function (currentChartId) {
+            var currentChart = this.charts[currentChartId - 1],
+                d3Chart      = d3.select(".chart-id-" + currentChartId + " svg"),
+                d3ChartData  = d3Chart.data();
+
+            if (d3ChartData[0]) {
+                d3ChartData[0][d3ChartData[0].length - 1].disabled = this.disableCapacity;
+            }
+
+            currentChart.update();
+        },
+
+        updateBrush: function (currentChartId) {
+            if (window.localSettings !== undefined) {
+                this.globalBrushExtent = window.localSettings.get('global_brush_extent');
+            }
+
+            if (this.globalBrushExtent) {
+                this.setBrushExtent();
+                this.reloadChart(currentChartId);
+            }
+        },
+
+        reloadChart: function (currentChartId) {
+            var currentChart = this.charts[currentChartId - 1];
+            if (currentChart && currentChart.update) {
+                currentChart.update();
+            }
+        },
+
+        setBrushExtent: function () {
+            this.charts.forEach(function (chart) {
+                if (chart) {
+                    chart.brushExtent(this.globalBrushExtent);
+                }
+            });
+        },
+
+        clearBrush: function () {
+            this.globalBrushExtent = undefined;
+            window.localSettings.remove('global_brush_extent');
+            this.charts.forEach(function (chart) {
+                if (chart) {
+                    d3.select(".brush").call(chart.brush.clear());
+                    chart.brushExtent([0, 0]);
+                    chart.update();
+                }
+            });
+        },
+
+        formatDate: function (date) {
+            var monthNames = [
+                "January", "February", "March",
+                "April", "May", "June", "July",
+                "August", "September", "October",
+                "November", "December"
+            ];
+
+            return [date.getDate(), monthNames[date.getMonth()]].join(" ");
+        }
     };
-
-    currentChart.update();
-  },
-
-  updateBrush: function(currentChartId){
-    if(typeof(localSettings) !== 'undefined'){
-      this.globalBrushExtent = localSettings.get('global_brush_extent')
-    };
-
-    if(this.globalBrushExtent){
-      this.setBrushExtent();
-      this.reloadChart(currentChartId);
-    }
-  },
-
-  reloadChart: function(currentChartId){
-    var currentChart = this.charts[currentChartId - 1];
-    if(currentChart && currentChart.update){
-      currentChart.update();
-    };
-  },
-
-  setBrushExtent: function(){
-    for(var i = 0; i < this.charts.length; i++){
-      if(this.charts[i]){
-        this.charts[i].brushExtent(this.globalBrushExtent);
-      }
-    };
-  },
-
-  clearBrush: function(){
-    this.globalBrushExtent = undefined;
-    for(var i = 0; i < this.charts.length; i++){
-      if(this.charts[i]){
-        d3.select(".brush").call(this.charts[i].brush.clear());
-        this.charts[i].brushExtent([0,0]);
-        this.charts[i].update();
-      }
-    };
-  },
-
-  formatDate: function(date){
-    var monthNames = [
-      "January", "February", "March",
-      "April", "May", "June", "July",
-      "August", "September", "October",
-      "November", "December"
-    ];
-
-    return [date.getDate(), monthNames[date.getMonth()]].join(" ");
-  }
-};
+}());
--- app/assets/javascripts/charts/helpers/local_settings.js
+++ app/assets/javascripts/charts/helpers/local_settings.js
@@ -1,45 +1,52 @@
-var LocalSettings = (function(){
-  LocalSettings.prototype = {
-    set: function(key, value){
-      var settings = read.call(this);
-      settings[key] = value;
-      write.call(this, settings);
-    },
+var LocalSettings = (function () {
+    'use strict';
 
-    get: function(key){
-      return read.call(this)[key];
-    },
+    LocalSettings.prototype = {
+        set: function (key, value) {
+            var settings = read.call(this);
+            settings[key] = value;
+            write.call(this, settings);
+        },
 
-    getAll: function(){
-      return read.call(this);
-    }
-  };
+        get: function (key) {
+            return read.call(this)[key];
+        },
 
-  function write(value){
-    if(isSupportingLocalStorage() && value !== null){
-      window.localStorage.setItem(this.testingGroundId, JSON.stringify(value));
-    }
-  };
+        getAll: function () {
+            return read.call(this);
+        },
 
-  function read(){
-    if(isSupportingLocalStorage()){
-      return JSON.parse(window.localStorage.getItem(this.testingGroundId));
+        remove: function (key) {
+            var localValue = this.getAll();
+            delete localValue[key];
+            write.call(this, localValue);
+        }
     };
-  };
 
-  function isSupportingLocalStorage(){
-    try {
-      return 'localStorage' in window && window['localStorage'] !== null;
+    function write(value) {
+        if (isSupportingLocalStorage() && value !== null) {
+            window.localStorage.setItem(this.testingGroundId, JSON.stringify(value));
+        }
     }
-    catch(e) {
-      return false;
-    };
-  };
 
-  function LocalSettings(_testingGroundId){
-    this.testingGroundId = _testingGroundId;
-    write.call(this, read.call(this) || {});
-  };
+    function read() {
+        if (isSupportingLocalStorage()) {
+            return JSON.parse(window.localStorage.getItem(this.testingGroundId));
+        }
+    }
 
-  return LocalSettings;
-})();
+    function isSupportingLocalStorage() {
+        try {
+            return 'localStorage' in window && window.localStorage !== null;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    function LocalSettings(_testingGroundId) {
+        this.testingGroundId = _testingGroundId;
+        write.call(this, read.call(this) || {});
+    }
+
+    return LocalSettings;
+}());
