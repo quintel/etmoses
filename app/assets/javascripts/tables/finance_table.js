b6c6b0340099767ea537e12367877fa1b2f176d9
Gerard Westerhof <gerard@grdw.nl>
2015-10-20 12:11:09 +0200

    Fixes concurrency #545

--- app/assets/javascripts/tables/finance_table.js
+++ app/assets/javascripts/tables/finance_table.js
@@ -1,92 +1,94 @@
-var FinanceTable = (function(){
-  var financeTable, incomingTotal, outgoingTotal, currentFinances, financeArea,
-      freeformRow, rowHeight;
+var FinanceTable = (function() {
+    var financeTable, incomingTotal, outgoingTotal, currentFinances, financeArea,
+        freeformRow, rowHeight;
 
-  FinanceTable.prototype = {
-    create: function(){
-      financeArea     = $("form #business_case_financials");
-      rowHeight       = financeTable.find("tbody tr.stakeholder").length;
-      currentFinances = JSON.parse(financeArea.text());
-      freeformRow     = (currentFinances[rowHeight] || { freeform: [] });
+    FinanceTable.prototype = {
+        create: function() {
+            financeArea = $("form #business_case_financials");
+            rowHeight = financeTable.find("tbody tr.stakeholder").length;
+            currentFinances = JSON.parse(financeArea.text());
+            freeformRow = (currentFinances[rowHeight] || {
+                freeform: []
+            });
 
-      calculateTotals();
+            calculateTotals();
 
-      $(financeTable).find("td.row_value input").on('change', calculateTotals);
-    }
-  };
-
-  function calculateTotals(){
-    financeTable.find("tbody tr.stakeholder").each(calculateIncomingRowTotals);
-    financeTable.find("tbody tr").each(calculateOutgoingRowTotals);
-
-    updateCurrentFinances();
-  };
-
-  function calculateIncomingRowTotals(i){
-    incomingTotal = calculateIncomingTotal.call(this);
-
-    setTotalAndFormat.call($(this).find("td.incoming_total"), incomingTotal);
-  };
-
-  function calculateIncomingTotal(){
-    var incoming = 0;
-    $(this).find("td:not(.center) span").each(function(){
-      incoming += parseFloat($(this).data('value'));
-    });
-    return incoming;
-  };
-
-  function calculateOutgoingRowTotals(i){
-    outgoing = calculateOutgoing.call(this, i);
-    freeform = calculateFreeform.call(this, i);
-
-    total = outgoing + freeform;
-
-    setTotalAndFormat.call($(financeTable.find("td.total")[i]), total);
-
-    if(!$(this).hasClass("freeform")){
-      freeformRow.freeform[i] = freeform;
+            $(financeTable).find("td.row_value input").on('change', calculateTotals);
+        }
     };
-  };
 
-  function calculateOutgoing(i){
-    return calculateVertically.call(this, i, function(){
-      return this.find("span").data("value");
-    });
-  };
+    function calculateTotals() {
+        financeTable.find("tbody tr.stakeholder").each(calculateIncomingRowTotals);
+        financeTable.find("tbody tr").each(calculateOutgoingRowTotals);
 
-  function calculateFreeform(i){
-    return calculateVertically.call(this, i, function(){
-      return this.find("input.free-form").val();
-    });
-  };
+        updateCurrentFinances();
+    };
 
-  function calculateVertically(i, findCellValue){
-    var calculation = 0;
+    function calculateIncomingRowTotals(i) {
+        incomingTotal = calculateIncomingTotal.call(this);
 
-    $(this).find("td").each(function(j){
-      var row  = $(financeTable.find("tbody tr")[j]);
-      var cell = $(row.find("td.row_value")[i]);
+        setTotalAndFormat.call($(this).find("td.incoming_total"), incomingTotal);
+    };
 
-      calculation += parseFloat(findCellValue.call(cell) || 0.0);
-    });
+    function calculateIncomingTotal() {
+        var incoming = 0;
+        $(this).find("td:not(.center) span").each(function() {
+            incoming += parseFloat($(this).data('value'));
+        });
+        return incoming;
+    };
 
-    return calculation;
-  };
+    function calculateOutgoingRowTotals(i) {
+        outgoing = calculateOutgoing.call(this, i);
+        freeform = calculateFreeform.call(this, i);
 
-  function setTotalAndFormat(total){
-    $(this).text(total);
-    $(this).formatCurrency(Finance.currencyOptions);
-  };
+        total = outgoing + freeform;
 
-  function updateCurrentFinances(){
-    currentFinances[rowHeight] = freeformRow;
-    financeArea.text(JSON.stringify(currentFinances));
-  };
+        setTotalAndFormat.call($(financeTable.find("td.total")[i]), total);
 
-  function FinanceTable(_financeTable){
-    financeTable = _financeTable;
-  };
+        if (!$(this).hasClass("freeform")) {
+            freeformRow.freeform[i] = freeform;
+        };
+    };
 
-  return FinanceTable;
+    function calculateOutgoing(i) {
+        return calculateVertically.call(this, i, function() {
+            return this.find("span").data("value");
+        });
+    };
+
+    function calculateFreeform(i) {
+        return calculateVertically.call(this, i, function() {
+            return this.find("input.free-form").val();
+        });
+    };
+
+    function calculateVertically(i, findCellValue) {
+        var calculation = 0;
+
+        $(this).find("td").each(function(j) {
+            var row = $(financeTable.find("tbody tr")[j]);
+            var cell = $(row.find("td.row_value")[i]);
+
+            calculation += parseFloat(findCellValue.call(cell) || 0.0);
+        });
+
+        return calculation;
+    };
+
+    function setTotalAndFormat(total) {
+        $(this).text(total);
+        $(this).formatCurrency(Finance.currencyOptions);
+    };
+
+    function updateCurrentFinances() {
+        currentFinances[rowHeight] = freeformRow;
+        financeArea.text(JSON.stringify(currentFinances));
+    };
+
+    function FinanceTable(_financeTable) {
+        financeTable = _financeTable;
+    };
+
+    return FinanceTable;
 })();
--- app/assets/javascripts/tables/market_model_table.js
+++ app/assets/javascripts/tables/market_model_table.js
@@ -1,24 +1,24 @@
-var MarketModelTable = (function(){
-  var editableTable;
+var MarketModelTable = (function() {
+    var editableTable;
 
-  function MarketModelTable(_selector){
-    editableTable = new EditableTable(_selector);
-  };
+    function MarketModelTable(_selector) {
+        editableTable = new EditableTable(_selector);
+    };
 
-  MarketModelTable.prototype = {
-    append: function(){
-      editableTable.append(this.updateTable);
-    },
+    MarketModelTable.prototype = {
+        append: function() {
+            editableTable.append(this.updateTable);
+        },
 
-    updateTable: function(){
-      $("#market_model_interactions").text(JSON.stringify(editableTable.getData()));
-    }
-  };
+        updateTable: function() {
+            $("#market_model_interactions").text(JSON.stringify(editableTable.getData()));
+        }
+    };
 
-  return MarketModelTable;
+    return MarketModelTable;
 })();
 
-$(document).on("page:change", function(){
-  window.currentMarketTable = new MarketModelTable("table.table.interactions");
-  window.currentMarketTable.append();
+$(document).on("page:change", function() {
+    window.currentMarketTable = new MarketModelTable("table.table.interactions");
+    window.currentMarketTable.append();
 });
