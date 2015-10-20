8daceacd7eec8b7d46f5fa94a1389aa789017f12
Gerard Westerhof <gerard@grdw.nl>
2015-10-20 12:11:09 +0200

    Fixes concurrency #545

--- app/assets/javascripts/tables/profile_table.js
+++ app/assets/javascripts/tables/profile_table.js
@@ -1,63 +1,66 @@
-var ProfileTable = (function(){
-  var widget, selector, profileSelectBoxes, edsnSwitch;
+/*global document,EditableTable,EdsnSwitch,ETHelper,ProfileSelectBoxes*/
+var ProfileTable = (function () {
+    'use strict';
 
-  ProfileTable.prototype = {
-    append: function(){
-      profileSelectBoxes.add();
-      edsnSwitch.enable();
+    function parseTableToJSON() {
+        var tableProfile  = this.editableTable.getData(),
+            groupedByNode = ETHelper.groupBy(tableProfile, 'node');
 
-      editableTable.append(parseTableToJSON, changeData);
-      parseTableToJSON();
-    },
-
-    updateProfiles: function(){
-      profileSelectBoxes.update();
-      parseTableToJSON();
+        $("#technology_distribution").text(JSON.stringify(tableProfile));
+        $("#testing_ground_technology_profile").text(JSON.stringify(groupedByNode));
     }
-  };
 
-  function parseTableToJSON(){
-    var tableProfile = editableTable.getData();
-    var groupedByNode = ETHelper.groupBy(tableProfile, 'node');
-
-    $("#technology_distribution").text(JSON.stringify(tableProfile));
-    $("#testing_ground_technology_profile").text(JSON.stringify(groupedByNode));
-  };
-
-  function changeData(){
-    if(this.header == "name"){
-      this.tableData["name"] = getNameForType(this.attribute);
-      this.tableData["type"] = this.attribute;
+    function getNameForType(type) {
+        var selectedOption = $("select.name").first().find("option").filter(function () {
+            if (type === "base_load_edsn") {
+                type = "base_load";
+            }
+            return $(this).val() === type;
+        });
+        return selectedOption.text();
     }
-    else if(!(/demand|electrical_capacity/.test(this.header) && this.attribute == "")){
-      this.tableData[this.header] = this.attribute
+
+    function changeData() {
+        if (this.header === "name") {
+            this.tableData.name = getNameForType(this.attribute);
+            this.tableData.type = this.attribute;
+        } else if (!(/demand|electrical_capacity/.test(this.header) && this.attribute === "")) {
+            this.tableData[this.header] = this.attribute;
+        }
+    }
+
+    ProfileTable.prototype = {
+        append: function () {
+            this.profileSelectBoxes.add();
+            this.edsnSwitch.enable();
+            this.editableTable.append(parseTableToJSON.bind(this), changeData);
+
+            parseTableToJSON.call(this);
+        },
+
+        updateProfiles: function () {
+            this.profileSelectBoxes.update();
+
+            parseTableToJSON.call(this);
+        }
     };
-  };
 
-  function getNameForType(type){
-    var selectedOption = $("select.name").first().find("option").filter(function(){
-      if(type == "base_load_edsn"){
-        type = "base_load";
-      };
-      return $(this).val() == type;
-    });
-    return selectedOption.text();
-  };
+    function ProfileTable(selector) {
+        this.selector           = selector;
+        this.editing            = $("form.edit_testing_ground").length > 0;
+        this.edsnSwitch         = new EdsnSwitch(this.editing);
+        this.profileSelectBoxes = new ProfileSelectBoxes(this.edsnSwitch);
+        this.editableTable      = new EditableTable(this.selector);
+    }
 
-  function ProfileTable(_selector){
-    selector           = _selector;
-    editing            = $("form.edit_testing_ground").length > 0;
-    edsnSwitch         = new EdsnSwitch(editing);
-    profileSelectBoxes = new ProfileSelectBoxes(edsnSwitch);
-    editableTable      = new EditableTable(_selector);
-  };
+    return ProfileTable;
+}());
 
-  return ProfileTable;
-})();
+$(document).on("page:change", function () {
+    'use strict';
 
-$(document).on("page:change", function(){
-  if($("#profiles-table > table").length > 0){
-    window.currentProfileTable = new ProfileTable("#profiles-table > table");
-    window.currentProfileTable.append();
-  }
+    if ($("#profiles-table > table").length > 0) {
+        window.currentProfileTable = new ProfileTable("#profiles-table > table");
+        window.currentProfileTable.append();
+    }
 });
