3b138e6eae0b8ae9b89fc2c7ee8ef221940b535f
Gerard Westerhof <gerard@grdw.nl>
2015-10-21 11:29:15 +0200

    Fixes defaults bug
    Fixes: #558

--- app/assets/javascripts/edsn.js
+++ app/assets/javascripts/edsn.js
@@ -1,47 +1,48 @@
 var EDSN_THRESHOLD = 30;
 
-var EdsnSwitch = (function(){
-  var editing;
-  var validBaseLoads = /^(base_load|base_load_edsn)$/;
+var EdsnSwitch = (function() {
+    var editing;
+    var validBaseLoads = /^(base_load|base_load_edsn)$/;
 
-  EdsnSwitch.prototype = {
-    enable: function(){
-      if(editing){
-        swapEdsnBaseLoadSelectBoxes();
-      }
-    },
+    EdsnSwitch.prototype = {
+        enable: function() {
+            if (editing) {
+                swapEdsnBaseLoadSelectBoxes();
+            }
+        },
 
-    isEdsn: function(){
-      return validBaseLoads.test($(this).val());
-    },
+        isEdsn: function() {
+            return validBaseLoads.test($(this).val());
+        },
 
-    cloneAndAppendProfileSelect: function(){
-      swapSelectBox.call(this);
-    }
-  };
+        cloneAndAppendProfileSelect: function() {
+            swapSelectBox.call(this);
+        }
+    };
 
-  function swapEdsnBaseLoadSelectBoxes(){
-    $("tr.base_load_edsn select.name").each(swapSelectBox);
-  };
+    function swapEdsnBaseLoadSelectBoxes() {
+        $("tr.base_load_edsn select.name").each(swapSelectBox);
+    };
 
-  function swapSelectBox(){
-    var technology   = $(this).val();
-    var self         = this;
-    var unitSelector = $(this).parents("tr").find(".units input");
-    var units        = parseInt(unitSelector.val());
-    var actual       = (units > EDSN_THRESHOLD ? "base_load_edsn" : "base_load");
-    var select       = $(".hidden select." + actual).clone(true, true);
+    function swapSelectBox() {
+        var technology = $(this).val();
+        var self = this;
+        var unitSelector = $(this).parents("tr").find(".units input");
+        var units = parseInt(unitSelector.val());
+        var actual = (units > EDSN_THRESHOLD ? "base_load_edsn" : "base_load");
+        var select = $(".hidden select." + actual).clone(true, true);
 
-    $(this).parent().next().html(select);
-    $(this).find("option[value='" + technology + "']").attr('value', actual);
+        $(this).parent().next().html(select);
+        $(this).find("option[value='" + technology + "']").attr('value', actual);
 
-    unitSelector.off('change').on('change', swapSelectBox.bind(self));
-  };
+        unitSelector.off('change').on('change', swapSelectBox.bind(self));
 
-  function EdsnSwitch(_editing){
-    editing = _editing;
-  };
+        return select;
+    };
 
-  return EdsnSwitch;
+    function EdsnSwitch(_editing) {
+        editing = _editing;
+    };
+
+    return EdsnSwitch;
 })();
-
--- app/assets/javascripts/tables/profile_select_boxes.js
+++ app/assets/javascripts/tables/profile_select_boxes.js
@@ -1,52 +1,108 @@
-var ProfileSelectBoxes = (function() {
-    var etmDefaults, edsnSwitch;
-    var isChanged = false;
-    var defaultValues = {
-        defaultCapacity: null,
-        defaultDemand: null,
-        defaultVolume: null
-    };
-    var attributes = {
-        technical_lifetime: null,
-        initial_investment: null,
-        full_load_hours: null,
-        om_costs_per_year: null,
-        om_costs_per_full_load_hour: null,
-        om_costs_for_ccs_per_full_load_hour: null
-    };
+var ProfileSelectBoxes = (function () {
+    'use strict';
 
-    ProfileSelectBoxes.prototype = {
-        add: function() {
-            addProfileSelectBoxes();
-            addChangeListenerToNameBox();
-            addChangeListenerToProfileBox();
+    var etmDefaults, edsnSwitch,
+        isChanged = false,
+        defaultValues = {
+            defaultCapacity: null,
+            defaultDemand: null,
+            defaultVolume: null
         },
-
-        update: function() {
-            updateSelectBoxes();
-        }
-    };
+        attributes = {
+            technical_lifetime: null,
+            initial_investment: null,
+            full_load_hours: null,
+            om_costs_per_year: null,
+            om_costs_per_full_load_hour: null,
+            om_costs_for_ccs_per_full_load_hour: null
+        };
 
     function updateSelectBoxes() {
-        $("select.name").each(function() {
+        $("select.name").each(function () {
             $(this).parent().next().find("select").val($(this).data('profile'));
         });
-    };
+    }
+
+    function getDefaults() {
+        var selectedOption = $(this).find("option[value='" + $(this).val() + "']");
+
+        return selectedOption.data() || defaultValues;
+    }
+
+    function defaultsFromEtm(tech) {
+        return etmDefaults[tech] ? etmDefaults[tech][0] : {};
+    }
+
+    function setCellDefault() {
+        var inputField = $(this.techBox).parents("tr").find('.' + this.key + " input"),
+            technology = $(this.techBox).val(),
+            etmValue   = defaultsFromEtm(technology)[this.key],
+            userInput  = parseFloat(inputField.val());
+
+        if (etmValue === userInput || isChanged) {
+            userInput = undefined;
+        }
+
+        inputField.val(userInput || etmValue ||
+            this.profileDefault || this.techDefault || '');
+    }
+
+    function updateTextCells(profileSelectbox) {
+        var key,
+            defaultValue,
+            technologyDefaults = getDefaults.call(this),
+            profileDefaults = getDefaults.call(profileSelectbox);
+
+        for (defaultValue in defaultValues) {
+            key = defaultValue.replace(/default/, '').toLowerCase();
+
+            if (key === 'capacity') {
+                key = 'electrical_capacity';
+            }
+
+            setCellDefault.call({
+                techBox: this,
+                key: key,
+                techDefault: technologyDefaults[defaultValue],
+                profileDefault: profileDefaults[defaultValue]
+            });
+        }
+    }
+
+    function defaultCloneAndAppend() {
+        var technology = $(this).val(),
+            profileSelectbox = $(".hidden.profile select." + technology).clone(true, true);
+
+        $(this).parents("tr").find(".units input").off("change");
+        $(this).parent().next().html(profileSelectbox);
+
+        updateTextCells.call(this, profileSelectbox);
+
+        if (!isChanged) {
+            profileSelectbox.val($(this).data('profile'));
+        }
+    }
+
+    function cloneAndAppendProfileSelect() {
+        if (edsnSwitch.isEdsn.call(this)) {
+            updateTextCells.call(this,
+                edsnSwitch.cloneAndAppendProfileSelect.call(this));
+        } else {
+            defaultCloneAndAppend.call(this);
+        }
+    }
 
     function addProfileSelectBoxes() {
-        $("select.name").each(function() {
+        $("select.name").each(function () {
             cloneAndAppendProfileSelect.call(this);
         });
-    };
+    }
 
-    function addChangeListenerToNameBox() {
-        $("select.name").off("change").on("change", function() {
-            isChanged = true;
-
-            fetchEtmAttributes.call(this);
-            cloneAndAppendProfileSelect.call(this);
-        });
-    };
+    function setEtmAttributes(data) {
+        for (var key in attributes) {
+            $(this).parents("tr").find("." + key + " input").val(data[key]);
+        }
+    }
 
     function fetchEtmAttributes() {
         if (/^(base_load|generic)/.test($(this).val())) {
@@ -62,101 +118,51 @@ var ProfileSelectBoxes = (function() {
             },
             success: setEtmAttributes.bind(this)
         });
-    };
+    }
 
-    function setEtmAttributes(data) {
-        for (var key in attributes) {
-            $(this).parents("tr").find("." + key + " input").val(data[key]);
-        };
-    };
+
+    function addChangeListenerToNameBox() {
+        $("select.name").off("change").on("change", function () {
+            isChanged = true;
+
+            fetchEtmAttributes.call(this);
+            cloneAndAppendProfileSelect.call(this);
+        });
+    }
 
     function addChangeListenerToProfileBox() {
-        $("td.profile select").off().on("change", function() {
+        $("td.profile select").off().on("change", function () {
             updateTextCells(this, $(this).parents("tr"));
         });
-    };
-
-    function cloneAndAppendProfileSelect() {
-        if (edsnSwitch.isEdsn.call(this)) {
-            edsnSwitch.cloneAndAppendProfileSelect.call(this);
-        } else {
-            defaultCloneAndAppend.call(this);
-        }
-    };
-
-    function defaultCloneAndAppend() {
-        var technology = $(this).val();
-        var profileSelectbox = $(".hidden.profile select." + technology).clone(true, true);
-
-        $(this).parents("tr").find(".units input").off("change");
-        $(this).parent().next().html(profileSelectbox);
-
-        updateTextCells.call(this, profileSelectbox);
-
-        if (!isChanged) {
-            profileSelectbox.val($(this).data('profile'));
-        };
-    };
-
-    function updateTextCells(profileSelectbox) {
-        var technologyDefaults = getDefaults.call(this);
-        var profileDefaults = getDefaults.call(profileSelectbox);
-
-        var key;
-
-        for (var defaultValue in defaultValues) {
-            key = defaultValue.replace(/default/, '').toLowerCase();
-
-            if (key === 'capacity') {
-                key = 'electrical_capacity';
-            }
-
-            setCellDefault.call({
-                techBox: this,
-                key: key,
-                techDefault: technologyDefaults[defaultValue],
-                profileDefault: profileDefaults[defaultValue]
-            });
-        };
-    };
-
-    function getDefaults() {
-        var selectedOption = $(this).find("option[value='" + $(this).val() + "']");
-
-        return selectedOption.data() || defaultValues;
-    };
-
-    function setCellDefault() {
-        var inputField = $(this.techBox).parents("tr").find('.' + this.key + " input");
-        var technology = $(this.techBox).val();
-        var etmValue = defaultsFromEtm(technology)[this.key];
-        var userInput = parseFloat(inputField.val());
-
-        if (etmValue == userInput || isChanged) userInput = undefined;
-
-        inputField.val(userInput || etmValue ||
-            this.profileDefault || this.techDefault || '');
-    };
-
-    function defaultsFromEtm(tech) {
-        return etmDefaults[tech] ? etmDefaults[tech][0] : {};
-    };
+    }
 
     function setEtmDefaults() {
         var profile = JSON.parse($("#testing_ground_technology_profile").text());
         var technologies = []
 
-        Object.keys(profile).map(function(key) {
+        Object.keys(profile).map(function (key) {
             technologies = technologies.concat(profile[key])
         });
 
         return ETHelper.groupBy(technologies, 'type');
+    }
+
+    ProfileSelectBoxes.prototype = {
+        add: function() {
+            addProfileSelectBoxes();
+            addChangeListenerToNameBox();
+            addChangeListenerToProfileBox();
+        },
+
+        update: function() {
+            updateSelectBoxes();
+        }
     };
 
-    function ProfileSelectBoxes(_edsnSwitch) {
+    function ProfileSelectBoxes(thisEdsnSwitch) {
         etmDefaults = setEtmDefaults();
-        edsnSwitch = _edsnSwitch;
-    };
+        edsnSwitch  = thisEdsnSwitch;
+    }
 
     return ProfileSelectBoxes;
-})();
+}());
