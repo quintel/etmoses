513bfdabadece0ee6d59fbb7706aa6bf1a41ad35
Anthony Williams <hi@antw.io>
2015-10-27 09:57:42 +0000

    Fix flex-realised specs >.<

--- spec/models/market/measures/flexibility_realised_spec.rb
+++ spec/models/market/measures/flexibility_realised_spec.rb
@@ -5,6 +5,7 @@ module Market::Measures
     let(:node)     { Network::Node.new(:node, resolution: 1) }
     let(:variant)  { Network::Node.new(:node, resolution: 1) }
     let(:variants) { { basic: ->*{ variant } } }
+    let(:flex)     { FlexibilityRealised.call(node, variants) }
 
     context 'given a node with loads [1, 2, 1, 3]' do
       before  { node.set(:load, [1.0, 2.0, 1.0, 3.0]) }
@@ -12,8 +13,8 @@ module Market::Measures
       context 'and variant of [2, 2, 3, 3]' do
         before  { variant.set(:load, [2.0, 2.0, 3.0, 3.0]) }
 
-        it 'has total flexibility of 3 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to eq(3)
+        it 'has total flexibility of 1, 0, 2, 0 kWh' do
+          expect(flex).to eq([1, 0, 2, 0])
         end
       end # and variant of [2, 2, 3, 3]
 
@@ -21,7 +22,7 @@ module Market::Measures
         before  { variant.set(:load, [1.0, 2.0, 1.0, 3.0]) }
 
         it 'has total flexibility of 0 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to be_zero
+          expect(flex).to eq([0, 0, 0, 0])
         end
       end # and variant of [1, 2, 1, 3]
 
@@ -29,7 +30,7 @@ module Market::Measures
         before  { variant.set(:load, [-1.0, -2.0, -2.0, -3.0]) }
 
         it 'has total flexibility of 1 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to eq(1)
+          expect(flex).to eq([0, 0, 1, 0])
         end
       end # and variant of [-1, -2, -2, -3]
     end # given a node with loads [1, 2, 1, 3]
@@ -41,7 +42,7 @@ module Market::Measures
         before  { variant.set(:load, [-2.0, -2.0, -3.0, -3.0]) }
 
         it 'has total flexibility of 3 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to eq(3)
+          expect(flex).to eq([1, 0, 2, 0])
         end
       end # and variant of [-2, -2, -3, -3]
 
@@ -49,7 +50,7 @@ module Market::Measures
         before  { variant.set(:load, [-1.0, -2.0, -1.0, -3.0]) }
 
         it 'has total flexibility of 0 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to be_zero
+          expect(flex).to eq([0, 0, 0, 0])
         end
       end # and variant of [-1, -2, -1, -3]
 
@@ -57,7 +58,7 @@ module Market::Measures
         before  { variant.set(:load, [1.0, 2.0, 2.0, 3.0]) }
 
         it 'has total flexibility of 1 kWh' do
-          expect(FlexibilityRealised.call(node, variants)).to eq(1)
+          expect(flex).to eq([0, 0, 1, 0])
         end
       end # and variant of [1, 2, 2, 3]
     end # given a node with loads [-1, -2, -1, -3]
