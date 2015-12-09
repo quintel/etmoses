dc326fb173304e03dac4d88faddd775a1444749b
Anthony Williams <hi@antw.io>
2015-12-09 14:08:11 +0000

    Fix $.fn.formatCurrency
    $.fn methods are given an array of matching elements; iterate through
    each DOM element in turn.

--- app/assets/javascripts/extensions.js
+++ app/assets/javascripts/extensions.js
@@ -1,36 +1,64 @@
 $.fn.extend({
+    formatCurrency: function() {
+        'use strict';
+
+        $(this).each(function(index, element) {
+            element = $(element);
+
+            var formatted = (parseFloat(element.text()) || 0.0).toFixed(2).
+                replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+
+
+            if (formatted[0] === '-') {
+                formatted = '-€' + formatted.slice(1, formatted.length);
+            } else {
+                formatted = '€' + formatted;
+            }
+
+            element.text(formatted);
+        });
+    },
+
     outerHTML: function (s) {
         'use strict';
 
         return s
             ? this.before(s).remove()
-            : jQuery("<p>").append(this.eq(0).clone()).html();
+            : $("<p>").append(this.eq(0).clone()).html();
     },
 
-    underscorizedData: function(){
+    underscorizedData: function () {
+        'use strict';
+
         var u = $(this).data(),
             newObject = {},
             keys = Object.keys(u);
 
-        $.each(keys, function(){
+        $.each(keys, function () {
             newObject[this.underscorize()] = u[this];
         });
 
         return newObject;
     },
 
-    selectedOption: function(value){
+    selectedOption: function (value) {
+        'use strict';
+
         return $(this).find("option[value='" + (value || $(this).val()) + "']");
     },
 
-    set: function(option, value){
+    set: function (option, value) {
+        'use strict';
+
         $(this).data(option, value)
                .attr("data-" + option.underscorize().replace(/\_/g, '-'), value);
     }
 });
 
-String.prototype.underscorize = function(){
-    return this.replace(/([A-Z])/g, function(a){
-        return "_"+a.toLowerCase();
+String.prototype.underscorize = function () {
+    'use strict';
+
+    return this.replace(/([A-Z])/g, function (a) {
+        return "_" + a.toLowerCase();
     });
-}
+};
