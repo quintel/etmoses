611a06fe78346a5e0e490912b29155a47cc8765a
Gerard Westerhof <gerard@grdw.nl>
2015-12-16 17:11:37 +0100

    Make it possible to change your own password + name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :authenticate_user!
+  skip_before_filter :authenticate_user!, only: %i(new create)
 
   def new
     @registration_form = RegistrationForm.new(params[:registration_form])
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
     else
       flash[:alert] = @registration_form.errors.full_messages.join(", ")
       render :new
+    end
+  end
+
+  def edit
+    @users_form = UsersForm.new(current_user.attributes.slice("email", "name"))
+  end
+
+  def update
+    @users_form = UsersForm.new(params[:users_form])
+    @users_form.user = current_user
+
+    if @users_form.submit
+      redirect_to edit_user_registration_path
+    else
+      flash[:alert] = @users_form.errors.full_messages.join(", ")
+      render :edit
     end
   end
 end
