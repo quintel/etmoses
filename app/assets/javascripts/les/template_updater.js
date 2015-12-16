645c8a78a497c4c2444d2352e6f33de355b29ed7
Gerard Westerhof <gerard@grdw.nl>
2015-12-16 12:09:17 +0100

    Adding undefined and blank to ignored techs

--- app/assets/javascripts/les/template_updater.js
+++ app/assets/javascripts/les/template_updater.js
@@ -17,8 +17,14 @@ var TemplateUpdater = (function () {
      *    The attributes are camelcased due to HTML5 DOM API
      */
     var ignoredTechs = [
-            'base_load', 'base_load_edsn', 'base_load_buildings', 'generic',
-            'buffer_space_heating', 'buffer_water_heating',
+            undefined,
+            '',
+            'base_load',
+            'base_load_edsn',
+            'base_load_buildings',
+            'generic',
+            'buffer_space_heating',
+            'buffer_water_heating',
             'households_water_heater_hybrid_heatpump_air_water_electricity_electricity',
             'households_water_heater_hybrid_heatpump_air_water_electricity_gas',
             'households_space_heater_hybrid_heatpump_air_water_electricity_electricity',
