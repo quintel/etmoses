43b4b36e908b833466406d91cc3c7097e6fab523
Gerard Westerhof <gerard@grdw.nl>
2016-02-16 17:00:17 +0100

    Rename COP + assume COP is 1 when undefined

--- app/assets/javascripts/les/profile_matrix_editor/calculations.js
+++ app/assets/javascripts/les/profile_matrix_editor/calculations.js
@@ -2,7 +2,7 @@ var Calculations = (function () {
     return {
         calculateInputCapacity: function() {
             return (Math.round(
-                (this.capacity / this.performanceCoefficient) * 10000)
+                (this.capacity / (this.performanceCoefficient || 1)) * 10000)
                 / 10000);
         }
     };
--- app/assets/javascripts/les/profile_matrix_editor/technologies_form.js
+++ app/assets/javascripts/les/profile_matrix_editor/technologies_form.js
@@ -154,7 +154,7 @@ var TechnologiesForm = (function () {
     TechnologiesForm.prototype = {
         append: function () {
             $(".technologies .technology:not(.hidden)").each(function () {
-                new TemplateUpdater($(this)).updatePositionOfTechnology();
+                new TemplateUpdater($(this)).updateExisting();
                 new ProfileSelectBox(this).add(updateJSON);
                 new BufferSelectBox(this).add();
                 new BatteryTemplateUpdater({
