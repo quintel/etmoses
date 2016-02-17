33c751cf5e03fceb383bd45b284671f1c122dc08
Anthony Williams <hi@antw.io>
2016-02-17 09:27:24 +0000

    TreeSampler should always default to :low resolution
    Closes #793

--- app/services/testing_ground/tree_sampler.rb
+++ app/services/testing_ground/tree_sampler.rb
@@ -2,10 +2,10 @@ class TestingGround
   class TreeSampler
     RESOLUTION_LENGTH_LOW = 365
 
-    def self.sample(networks, resolution = :low, nodes = nil)
+    def self.sample(networks, resolution = nil, nodes = nil)
       Hash[networks.each_pair.map do |carrier, graph|
         NetworkCache::LoadSetter.set(graph, nodes) do |node|
-          downsample(node.load, resolution)
+          downsample(node.load, resolution || :low)
         end
 
         [carrier, GraphToTree.convert(graph)]
@@ -16,7 +16,7 @@ class TestingGround
       size = (node_load.length / RESOLUTION_LENGTH_LOW).floor
 
       if resolution.to_sym == :low && size > 0
-        node_load.each_slice(size).map{ |loads| loads.max_by(&:abs) }
+        node_load.each_slice(size).map { |loads| loads.max_by(&:abs) }
       else
         node_load
       end
