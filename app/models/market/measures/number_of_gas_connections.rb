14882b8f12e6ff1fcabde0a6caf3edfb1ff3a93d
Anthony Williams <hi@antw.io>
2016-04-06 13:29:12 +0100

    Remove unused debug from NumberOfGasConnections

--- app/models/market/measures/number_of_gas_connections.rb
+++ app/models/market/measures/number_of_gas_connections.rb
@@ -1,7 +1,6 @@
 module Market::Measures
   module NumberOfGasConnections
     def self.call(node, variants)
-      # Rails.logger.info ["=============", variants[:gas].call].inspect
       if variants[:gas].call
         NumberOfConnections.call(node)
       else
