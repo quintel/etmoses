5e430ac3269efa8d417fcd0a7587b42fda3d133a
Gerard Westerhof <gerard@grdw.nl>
2016-04-07 11:02:37 +0200

    Adding NPV graph and a cumulative costs graph

--- spec/controllers/gas_asset_lists_controller_spec.rb
+++ spec/controllers/gas_asset_lists_controller_spec.rb
@@ -4,26 +4,25 @@ RSpec.describe GasAssetListsController do
   let(:user) { FactoryGirl.create(:user) }
   let(:testing_ground) { FactoryGirl.create(:testing_ground, user: user) }
   let!(:sign_in_user) { sign_in(user) }
+  let(:asset_list) {
+    [
+      { "pressure_level"=>"0.1",
+        "part"=>"connectors",
+        "type"=>"ye_olde_connector",
+        "amount"=>"1",
+        "stakeholder"=>"cooperation",
+        "building_year"=>"1980" },
+      { "pressure_level"=>"0.1",
+        "part"=>"pipes",
+        "type"=>"magic_pipe",
+        "amount"=>"1",
+        "stakeholder"=>"cooperation",
+        "building_year"=>"1960" }
+    ]
+  }
 
   describe "editing an existing gas list" do
     let(:gas_asset_list) { FactoryGirl.create(:gas_asset_list, testing_ground: testing_ground) }
-    let(:asset_list) {
-      [
-        { "pressure_level"=>"0.1",
-          "part"=>"connectors",
-          "type"=>"ye_olde_connector",
-          "amount"=>"1",
-          "stakeholder"=>"cooperation",
-          "building_year"=>"1960" },
-        { "pressure_level"=>"0.1",
-          "part"=>"pipes",
-          "type"=>"magic_pipe",
-          "amount"=>"1",
-          "stakeholder"=>"cooperation",
-          "building_year"=>"1960" }
-      ]
-    }
-
     it "updates an gas asset list" do
       post :update, id: gas_asset_list.id,
                     testing_ground_id: testing_ground.id,
@@ -55,6 +54,34 @@ RSpec.describe GasAssetListsController do
                        format: :json
 
       expect(JSON.parse(response.body)).to_not be_blank
+    end
+  end
+
+  describe "calculations" do
+    let(:gas_asset_list) {
+      FactoryGirl.create(:gas_asset_list,
+        testing_ground: testing_ground,
+        asset_list: asset_list)
+    }
+
+    it "calculates net present values" do
+      post :calculate_net_present_value,
+        testing_ground_id: testing_ground.id,
+        id: gas_asset_list.id
+
+      expect(JSON.parse(response.body).slice("1960", "2010")).to eq({
+        "1960" => 500.0, "2010" => 40.0
+      })
+    end
+
+    it "calculates cumulative investments" do
+      post :calculate_cumulative_investment,
+        testing_ground_id: testing_ground.id,
+        id: gas_asset_list.id
+
+      expect(JSON.parse(response.body).slice("2010", "2030")).to eq({
+        "2010" => 500.0, "2030" => 600.0
+      })
     end
   end
 end
