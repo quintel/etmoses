ef06a6aa36e54c6a669af18640c9b17d26829bf8
Anthony Williams <hi@antw.io>
2016-04-20 14:23:09 +0100

    Warn when no "static_data_path" is set
    Also uses YAML.load_file; the previous iteration was opening the YAML
    file but never closing it.

--- config/initializers/active_hash.rb
+++ config/initializers/active_hash.rb
@@ -6,11 +6,18 @@ Rails.application.config.to_prepare do
     }.freeze
   end
 
-  DATA_SOURCES.each_pair do |folder, static|
-    static.data = Dir["#{ Rails.root }/#{Settings.static_data_path}/#{ folder }/*.yml"].map do |path|
-      file = File.open(path)
+  data_path = Rails.root.join(Settings.static_data_path.to_s)
 
-      YAML.load(file.read).update(type: File.basename(file, ".*"))
+  # Sanity check.
+  if Settings.static_data_path.blank? || ! data_path.directory?
+    fail "Incorrect `static_data_path' setting; either no value is set in " \
+         "config/setting.yml or config/settings/#{ Rails.env }.yml, or the " \
+         "directory does not exist: #{ Settings.static_data_path.inspect }"
+  end
+
+  DATA_SOURCES.each_pair do |folder, static|
+    static.data = Dir[data_path.join("#{ folder }/*.yml")].map do |path|
+      YAML.load_file(path).update(type: File.basename(path, '.*'))
     end
   end
 end
