55ea87dbcee314ae22695b1203c1e227df5b014d
Anthony Williams <hi@antw.io>
2016-05-12 10:18:16 +0100

    Remove unused migration
    Migration is broken on the latest HEAD since ImportableAttribute no
    longer exists.

--- db/migrate/20160502145559_remove_technology_id_field_from_importable_attributes.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTechnologyIdFieldFromImportableAttributes < ActiveRecord::Migration
-  def change
-    remove_index :importable_attributes, :technology_id_and_name
-    remove_column :importable_attributes, :technology_id
-  end
-end
