de6686405775433e8462e6e265a73fbcf4610388
Anthony Williams <hi@antw.io>
2016-05-25 10:53:22 +0100

    No MySQL, "test" is not the same as "Test" ಠ_ಠ
    Closes #987

--- app/models/load_profile.rb
+++ app/models/load_profile.rb
@@ -7,7 +7,7 @@ class LoadProfile < ActiveRecord::Base
   has_many :load_profile_components, dependent: :destroy
   has_many :technology_profiles, dependent: :destroy
 
-  validates :key, presence: true, uniqueness: true
+  validates :key, presence: true, uniqueness: { case_sensitive: false }
 
   accepts_nested_attributes_for :technology_profiles,
     reject_if: :all_blank, allow_destroy: true
