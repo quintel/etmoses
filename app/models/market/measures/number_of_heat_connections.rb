0d9ee25a959b6b28ef812e48c0b4e3318d1f23eb
Anthony Williams <hi@antw.io>
2016-06-10 14:22:36 +0100

    Add NumberOfHeatConnections#count_with_technologies_list
    Given an array of InstalledTechnology objects, determines the number of
    heat connections installed.

--- app/models/market/measures/number_of_heat_connections.rb
+++ app/models/market/measures/number_of_heat_connections.rb
@@ -6,17 +6,27 @@ module Market::Measures
   module NumberOfHeatConnections
     CONNECTION_TECHS = %w(buffer_space_heating buffer_water_heating).freeze
 
-    def self.call(node, variants)
+    module_function
+
+    def call(node, variants)
       return 0 unless variants[:heat].call && node.get(:comps)
 
-      grouped = node.get(:comps).map(&:installed).group_by(&:type)
+      count_with_technologies_list(node.get(:comps).map(&:installed))
+    end
+
+    # Public: Given a list of technologies attached to an endpoint, returns how
+    # many heat connections the endpoint will have.
+    #
+    # Returns an Integer.
+    def count_with_technologies_list(list)
+      grouped = list.group_by(&:type)
 
       (CONNECTION_TECHS & grouped.keys).map do |type|
-        grouped[type].sum { |installed| installed.units }
+        grouped[type].sum { |installed| installed.units }.ceil
       end.max || 0
     end
 
-    def self.irregular?
+    def irregular?
       true
     end
   end
