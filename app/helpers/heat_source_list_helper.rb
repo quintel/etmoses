79250bb84f559ead64b036a475a7b1df2f196a84
Gerard Westerhof <gerard@grdw.nl>
2016-06-13 15:05:17 +0200

    Split up the dispatchable and must-run converters in select boxes

--- app/helpers/heat_source_list_helper.rb
+++ app/helpers/heat_source_list_helper.rb
@@ -1,10 +1,16 @@
 module HeatSourceListHelper
   def heat_source_technologies
-    Technology.heat_sources_for_table + [ Technology.generic ]
+    [ Technology.generic ] + Technology.heat_sources_for_table
   end
 
-  def options_for_heat_source_plant_types(key)
-    heat_sources = heat_source_technologies.map do |heat_source|
+  def heat_source_technologies_for(type)
+    heat_source_technologies.select do |tech|
+      type == :all || (tech.dispatchable == (type == :dispatchable))
+    end
+  end
+
+  def options_for_heat_source_plant_types(key, type = :all)
+    heat_sources = heat_source_technologies_for(type).map do |heat_source|
       [ I18n.t("heat_sources.#{ heat_source.key }"),
         heat_source.key ]
     end
