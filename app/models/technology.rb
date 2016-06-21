ee8ab48145196f99a14acf02be6f60920dd0bc39
Anthony Williams <hi@antw.io>
2016-06-21 16:33:35 +0100

    Technology#behavior should be a Symbol
    Ref #1100

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -96,6 +96,10 @@ class Technology < ActiveHash::Base
     attributes[:name] || key.humanize.to_s
   end
 
+  def behavior
+    (attributes[:behavior] || :generic).to_sym
+  end
+
   def importable?
     importable_attributes.any?
   end
