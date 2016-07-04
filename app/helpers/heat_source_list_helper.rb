a77f0365d50753aac5d5753abf5d9ed00ee27b93
Gerard Westerhof <gerard@grdw.nl>
2016-07-04 10:39:43 +0200

    Adding generic to must run technologies in heat asset table
    Fixes: #1113

--- app/helpers/heat_source_list_helper.rb
+++ app/helpers/heat_source_list_helper.rb
@@ -3,18 +3,27 @@ module HeatSourceListHelper
     [ Technology.generic ] + Technology.heat_sources_for_table
   end
 
-  def heat_source_technologies_for(type)
-    heat_source_technologies.select do |tech|
-      type == :all || (tech.dispatchable == (type == :dispatchable))
-    end
+  def heat_source_technologies_for(dispatchable = true)
+    heat_source_technologies.select { |tech| tech.dispatchable == dispatchable }
   end
 
-  def options_for_heat_source_plant_types(key, type = :all)
-    heat_sources = heat_source_technologies_for(type).map do |heat_source|
-      [ I18n.t("heat_sources.#{ heat_source.key }"),
-        heat_source.key ]
+  def create_options(technologies, key)
+    heat_sources = technologies.map do |heat_source|
+      [ I18n.t("heat_sources.#{ heat_source.key }"), heat_source.key ]
     end
 
     options_for_select(heat_sources, selected: key)
+  end
+
+  def options_for_must_run_heat_source_plant_types(key)
+    create_options(heat_source_technologies_for(false), key)
+  end
+
+  def options_for_dispatchable_heat_source_plant_types(key)
+    create_options(heat_source_technologies_for, key)
+  end
+
+  def options_for_heat_source_plant_types(key)
+    create_options(heat_source_technologies, key)
   end
 end
