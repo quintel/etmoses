eb24ebc3cb96c64e72c0f502e2bc5f83fb7f103b
Anthony Williams <hi@antw.io>
2016-07-09 10:48:42 +0100

    Store heat O&M costs as EUR/kW

--- app/models/import/fixed_om_costs_per_year_per_mw_attribute.rb
+++ app/models/import/fixed_om_costs_per_year_per_mw_attribute.rb
@@ -3,8 +3,7 @@ class Import
     Attribute.new(
       'om_costs_per_year',
       'fixed_operation_and_maintenance_costs_per_year_per_mw') do |value, _|
-        # kEUR/MW
-        (value / 1000).round(2)
+        (value / 1000).round(2) # EUR/MW to EUR/kW
       end
 end
 
