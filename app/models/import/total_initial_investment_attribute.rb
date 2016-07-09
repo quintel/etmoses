d3d7cc655f2bc192e2bbe18cd1d4ff573ae7a6e9
Anthony Williams <hi@antw.io>
2016-07-09 10:43:41 +0100

    Document total initial investment value

--- app/models/import/total_initial_investment_attribute.rb
+++ app/models/import/total_initial_investment_attribute.rb
@@ -2,7 +2,6 @@ class Import
   TotalInitialInvestmentAttribute = Attribute.new(
     'total_initial_investment', 'total_initial_investment_per_mw'
   ) do |value, _|
-    # Unit = kEUR/MW
-    (value / 1000).round(2)
+    (value / 1000).round(2) # EUR/MW to EUR/kW
   end
 end
