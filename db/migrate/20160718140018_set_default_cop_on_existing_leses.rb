41c11f5b8a8bd5822a75b535b562b807041efa67
Anthony Williams <hi@antw.io>
2016-07-18 16:27:22 +0100

    Show only whitelisted attributes on technology form
    Ref #1292

---
+++ db/migrate/20160718140018_set_default_cop_on_existing_leses.rb
@@ -0,0 +1,36 @@
+class SetDefaultCopOnExistingLeses < ActiveRecord::Migration
+  def up
+    count   = TestingGround.count
+    updated = 0
+
+    TestingGround.find_each.with_index do |les, index|
+      changed = false
+
+      les.technology_profile.each_tech do |tech|
+        next if tech.whitelisted?(:performance_coefficient)
+
+        cop = tech.performance_coefficient
+
+        if ! cop.nil? && cop.to_f != 1.0
+          changed = true
+          tech.performance_coefficient = 1.0
+        end
+      end
+
+      if changed
+        updated += 1
+        les.save(validate: false)
+      end
+
+      if ((index + 1) % 50).zero? || (index + 1) == count
+        puts "Done #{ index + 1 } of #{ count }"
+      end
+    end
+
+    puts "Finished. #{ updated } fixed."
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
