c3c3bcded7ad93ce48937560c1e33f13fa5709c3
Anthony Williams <hi@antw.io>
2016-08-17 12:32:41 +0100

    Remove logger statement from Buffer#store

--- app/models/network/technologies/buffer.rb
+++ app/models/network/technologies/buffer.rb
@@ -72,8 +72,7 @@ module Network
         false
       end
 
-      def store(frame, amount)
-        Rails.logger.info ['<<<', frame, amount]
+      def store(frame, amount)\
         stored.add(frame, amount * @installed.performance_coefficient)
       end
     end # Buffer
