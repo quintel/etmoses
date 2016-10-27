721aba381fc02db8880a69f2d6044b97edf8a220
Gerard Westerhof <gerard@grdw.nl>
2016-10-27 13:51:36 +0200

    Set 'edited' class when a new .csv file is selected
    Fixes: #1526

--- app/assets/javascripts/save_all.js
+++ app/assets/javascripts/save_all.js
@@ -31,12 +31,13 @@ var SaveAll = (function () {
         $(this).find("span.wait").removeClass("hidden");
     }
 
-    function change(e) {
-        var tabTarget = $(e.target).parent().attr("id"),
+    function change(e) {;
+        var tabTarget = $(e.target).parents(".tab-pane").attr("id"),
             tabHeader = $("ul.nav-tabs li a[href='#" + tabTarget + "']");
 
-        tabHeader.addClass("editing");
-        $(e.target).addClass("editing");
+        $(e.target).parents("form")
+            .add(tabHeader)
+            .addClass("editing");
     }
 
     function done(doneCallback) {
