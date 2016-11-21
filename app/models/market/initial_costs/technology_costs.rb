a2b9753f1122dbae3749da200a49855140bb257d
Anthony Williams <hi@antw.io>
2016-11-21 10:56:46 +0000

    Prevent double-counting hybrid technology costs
    Closes #1571

--- app/models/market/initial_costs/technology_costs.rb
+++ app/models/market/initial_costs/technology_costs.rb
@@ -2,11 +2,21 @@ module Market
   class InitialCosts
     class TechnologyCosts < Costs
       def calculate
+        seen = {}
+
         group_sum(technology_nodes) do |node|
-          technologies_for_node(node).sum do |tech|
-            # "Internal" technologies (such as the central heat buffer) will not
-            # define any cost methods.
-            tech.try(:total_yearly_costs)
+          if seen[node.key]
+            # If the same node exists in multiple networks, we don't need to
+            # calculate costs again.
+            0.0
+          else
+            seen[node.key] = true
+
+            technologies_for_node(node).sum do |tech|
+              # "Internal" technologies (such as the central heat buffer) will
+              # not define any cost methods.
+              tech.try(:total_yearly_costs)
+            end
           end
         end
       end
